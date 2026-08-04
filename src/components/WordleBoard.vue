aef747e5ec8862e7e585374918916808773bfe48
MiljanK <miljan.knezevic@elanwave.com>
2026-08-04 18:32:13 +0200

    Rename component

---
+++ src/components/WordleBoard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const props = defineProps({
+  msg: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+
+  <p>{{ props.msg }}</p>
+
+</template>
---
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest';
+import WordleBoard from "../WordleBoard.vue";
+import {mount} from "@vue/test-utils";
+
+describe('WordleBoard', () => {
+    it('renders correctly', () => {
+        const wrapper = mount(WordleBoard, {props: {msg: "Hello Vitest"}})
+        expect(wrapper.text()).toContain('Hello Vitest')
+    })
+})
