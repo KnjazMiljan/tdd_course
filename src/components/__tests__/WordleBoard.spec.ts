dd298aeb4ff57110552b2aed9078c70d74c6af03
MiljanK <miljan.knezevic@elanwave.com>
2026-08-05 07:28:46 +0200

    Add tests for defeat message

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -15,4 +15,18 @@ describe('WordleBoard', () => {
         // Assertion phase
         expect(wrapper.text()).toContain(VICTORY_MESSAGE);
     })
+
+    test('a defeat message appears when the user makes guess that is incorrect', async() => {
+        // Arrange phase
+        const wrapper = mount(WordleBoard, { props: { wordOfTheDay: "TESTS" }})
+
+        // Act phase
+        const guessInput = wrapper.find('input[type=text]');
+        await guessInput.setValue("WRONG");
+        await guessInput.trigger('keydown.enter');
+
+        // Assertion phase
+        expect(wrapper.text()).toContain("Better luck next time");
+    });
+    test.todo('no end-of-game message appears if the user has not yed made a guess');
 })
