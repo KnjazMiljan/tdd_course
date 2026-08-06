2d9eb34cc490222d5d6b70bf8d18effc7d2fccc7
MiljanK <miljan.knezevic@elanwave.com>
2026-08-06 07:33:55 +0200

    Fix the issue which allowed player to type guesses with numbers

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { DEFEAT_MESSAGE, VICTORY_MESSAGE, WORD_SIZE } from '@/settings'
 import englishWords from '@/englishWordsWith5Letters.json'
-import { computed, ref } from 'vue'
+import { computed, ref, triggerRef } from 'vue'
 
 defineProps({
   wordOfTheDay: {
@@ -12,32 +12,38 @@ defineProps({
   },
 })
 
-const guessInProgress = ref('')
+const guessInProgress = ref<string | null>(null)
 const guessSubmitted = ref('')
 
-const formattedGuessInProgress = computed({
+const formattedGuessInProgress = computed<string>({
   get() {
-    return guessInProgress.value
+    return guessInProgress.value ?? ''
   },
   set(rawValue: string) {
     guessInProgress.value = rawValue
       .replace(/[^A-Z]+/gi, '')
       .slice(0, WORD_SIZE)
       .toUpperCase()
+    triggerRef(formattedGuessInProgress)
   },
 })
 
 function onSubmit() {
-  if (!englishWords.includes(guessInProgress.value)) {
+  if (!englishWords.includes(formattedGuessInProgress.value)) {
     return
   }
-  guessSubmitted.value = guessInProgress.value
+  guessSubmitted.value = formattedGuessInProgress.value
 }
 </script>
 
 <template>
   <div>
-    <input v-model="formattedGuessInProgress" maxlength="5" type="text" @keydown.enter="onSubmit" />
+    <input
+      v-model="formattedGuessInProgress"
+      :maxlength="WORD_SIZE"
+      type="text"
+      @keydown.enter="onSubmit"
+    />
     <p
       v-if="guessSubmitted.length > 0"
       v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"
